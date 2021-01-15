c936e15b01209429bca14d08c6f3befcc7bed949
Greg Murrell <j_greg_m@comcast.net>
2021-01-15 17:21:27 -0500

    removed unncessary code

--- TechSupport/View/LoginForm.cs
+++ TechSupport/View/LoginForm.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TechSupport.View
@@ -50,7 +43,7 @@ namespace TechSupport.View
 
         private void closeAll(object sender, FormClosedEventArgs e)
         {
-            this.Close();
+            Environment.Exit(1);
         }
     }
 }
--- TechSupport/View/MainForm.cs
+++ TechSupport/View/MainForm.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TechSupport.View
@@ -27,7 +20,7 @@ namespace TechSupport.View
 
         private void closeAll(object sender, FormClosedEventArgs e)
         {
-            this.Close();
+            Environment.Exit(1);
         }
 
         private void lnklblLogout_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
