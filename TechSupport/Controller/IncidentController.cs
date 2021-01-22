a048994def6224f7a353b46c146c268d7797c6e6
Greg Murrell <j_greg_m@comcast.net>
2021-01-22 14:11:14 -0500

    DAL, Model, and Controller mostly finished

---
+++ TechSupport/Controller/IncidentController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechSupport.DAL;
+using TechSupport.Model;
+
+namespace TechSupport.Controller
+{
+    /// <summary>
+    /// Controller for the TechSupport program
+    /// </summary>
+    public class IncidentController
+    {
+        private IncidentDatabase database;
+
+        /// <summary>
+        /// Creates an IncidentController with a new IncidentDatabase
+        /// </summary>
+        public IncidentController()
+        {
+            this.database = new IncidentDatabase();
+        }
+
+        /// <summary>
+        /// Gets the List of Incidents
+        /// </summary>
+        /// <returns></returns>
+        public List<Incident> getIncidents()
+        {
+            return this.database.getIncidents();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="incident"></param>
+        public void Add(Incident incident)
+        {
+            if (incident == null)
+            {
+                throw new ArgumentNullException("incident", "Incident cannot be null");
+            }
+            this.database.Add(incident);
+        }
+    }
+}
---
+++ TechSupport/DAL/IncidentDatabase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using TechSupport.Model;
+
+namespace TechSupport.DAL
+{
+    /// <summary>
+    /// A mock-up of a DAL. Maintains a List of Incidents
+    /// </summary>
+    public class IncidentDatabase
+    {
+        private static List<Incident> _incidents = new List<Incident>
+        {
+            new Incident("Bing Virus", "Google keeps using this thing called Bing. Is that a virus?", 1),
+            new Incident("Printer On Fire", "It's on fire! Oh dear God!", 101)
+         };
+
+        /// <summary>
+        /// Gets the List of Incidents
+        /// </summary>
+        /// <returns></returns>
+        public List<Incident> getIncidents()
+        {
+            return _incidents;
+        }
+
+        /// <summary>
+        /// Adds an Incident to the List of Incidents
+        /// </summary>
+        /// <param name="incident"></param>
+        public void Add(Incident incident)
+        {
+            if (incident == null)
+            {
+                throw new ArgumentNullException("incident", "Incident cannot be null");
+            }
+            _incidents.Add(incident);
+        }
+    }
+}
