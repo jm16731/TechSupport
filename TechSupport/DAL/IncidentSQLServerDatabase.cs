a99d535da523a48c8dff5731b7cc83d6a5e7fb37
Greg Murrell <j_greg_m@comcast.net>
2021-02-06 11:55:25 -0500

    SqlServer connection

---
+++ TechSupport/DAL/IncidentSQLServerDatabase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechSupport.DAL
+{
+    class IncidentSQLServerDatabase
+    {
+        public static SqlConnection GetConnection()
+        {
+            //string connectionString =
+           //     "Data Source=localhost;Initial Catalog=TechSupport;" +
+           //     "Integrated Security=True";
+            //SqlConnection connection = new SqlConnection(connectionString);
+            return new SqlConnection("Data Source=localhost;Initial Catalog=TechSupport;Integrated Security=True");
+        }
+    }
+}
