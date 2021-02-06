a998903653f649337302712dc67919c6912e700b
Greg Murrell <j_greg_m@comcast.net>
2021-02-06 11:46:42 -0500

    UserControl for DisplayOpenIncidents

---
+++ TechSupport/UserControls/DisplayOpenIncidents.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace TechSupport.UserControls
+{
+    partial class DisplayOpenIncidents
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridViewIncidentViewer = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewIncidentViewer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridViewIncidentViewer
+            // 
+            this.gridViewIncidentViewer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridViewIncidentViewer.Location = new System.Drawing.Point(0, 3);
+            this.gridViewIncidentViewer.Name = "gridViewIncidentViewer";
+            this.gridViewIncidentViewer.RowHeadersWidth = 102;
+            this.gridViewIncidentViewer.RowTemplate.Height = 40;
+            this.gridViewIncidentViewer.Size = new System.Drawing.Size(1038, 746);
+            this.gridViewIncidentViewer.TabIndex = 0;
+            // 
+            // DisplayOpenIncidents
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.gridViewIncidentViewer);
+            this.Name = "DisplayOpenIncidents";
+            this.Size = new System.Drawing.Size(1041, 752);
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewIncidentViewer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gridViewIncidentViewer;
+    }
+}
---
+++ TechSupport/UserControls/DisplayOpenIncidents.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TechSupport.Controller;
+
+namespace TechSupport.UserControls
+{
+    public partial class DisplayOpenIncidents : UserControl
+    {
+        private IncidentController controller;
+        public DisplayOpenIncidents()
+        {
+            InitializeComponent();
+            this.controller = new IncidentController();
+        }
+
+        /// <summary>
+        /// Refreshes the source data
+        /// </summary>
+        public void RefreshDataGrid()
+        {
+            this.gridViewIncidentViewer.DataSource = null;
+            this.gridViewIncidentViewer.DataSource = this.controller.getIncidents();
+        }
+    }
+}
