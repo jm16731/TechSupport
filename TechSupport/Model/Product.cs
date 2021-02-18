03140c7d65a638bc5862fda694f831209340e7b6
Greg Murrell <j_greg_m@comcast.net>
2021-02-18 13:59:05 -0500

    proper data type for Version

--- TechSupport/Model/Product.cs
+++ TechSupport/Model/Product.cs
@@ -9,7 +9,7 @@ namespace TechSupport.Model
     {
         public string ProductCode { get; }
         public string Name { get; }
-        public string Version { get; }
+        public Decimal Version { get; }
         public DateTime ReleaseDate { get; }
 
         /// <summary>
@@ -19,16 +19,19 @@ namespace TechSupport.Model
         /// <param name="Name">Name of the product</param>
         /// <param name="Version">Version of the product</param>
         /// <param name="ReleaseDate">Release Date of the product</param>
-        public Product(string ProductCode, string Name, string Version, DateTime ReleaseDate)
+        public Product(string ProductCode, string Name, Decimal Version, DateTime ReleaseDate)
         {
-            if (string.IsNullOrEmpty(ProductCode) || string.IsNullOrEmpty(Name) || string.IsNullOrEmpty(Version))
+            if (string.IsNullOrEmpty(ProductCode) || string.IsNullOrEmpty(Name))
             {
-                throw new ArgumentException("product code " + "name " + "version", "Product Code, Name, and Version" +
-                    "cannot be null or empty");
+                throw new ArgumentException("product code " + "name", "Product Code and Name cannot be null or empty");
             }
             if (ReleaseDate == null)
             {
                 throw new ArgumentException("release date", "ReleaseDate cannot be null");
+            }
+            if (Version <= 0)
+            {
+                throw new ArgumentException("version", "Version cannot be zero or negative");
             }
 
             this.ProductCode = ProductCode;
