1f7fc97df92591e36420c2818bda0855e1944b1c
Greg Murrell <j_greg_m@comcast.net>
2021-03-04 16:01:02 -0500

    new query in get incidents by technician

--- TechSupport/Controller/IncidentController.cs
+++ TechSupport/Controller/IncidentController.cs
@@ -96,5 +96,15 @@ namespace TechSupport.Controller
             }
             return IncidentDAL.UpdateIncident(incidentID, description, technicianID);
         }
+
+        /// <see cref="IncidentDAL.GetIncidentsByTechnician(int)"/>
+        public List<Incident> GetIncidentsByTechnician(int techID)
+        {
+            if (techID < 1)
+            {
+                throw new ArgumentException("TechID must be greater than 0");
+            }
+            return IncidentDAL.GetIncidentsByTechnician(techID);
+        }
     }
 }
